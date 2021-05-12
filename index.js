15a5ec4417d907c890f71847e40e6c16c2dd1d17
Raghda  Muayad <therose000777@gmail.com>
2021-05-12 14:43:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,3 +20,63 @@ let allWagesFor = function () {
 
     return payable
 }
+
+
+S Advanced Functions: Context Lab
+You sent Today at 17:33
+timeInEvents: [],
+ timeOutEvents: []
+  }
+}
+
+let createEmployeeRecords = function(employeeRecordData){
+return employeeRecordData.map(function(record){
+return createEmployeeRecord(record)
+ })
+}
+
+let createTimeInEvent = function(dateTimeInStamp){
+let [date, hour] = dateTimeInStamp.split(' ');
+this.timeInEvents.push({
+ type: "TimeIn",
+  hour: parseInt(hour, 10),
+  date: date,
+    })
+  return this
+}
+let createTimeOutEvent = function(dateTimeOutStamp){
+    let [date, hour] = dateTimeOutStamp.split(' ');
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: date,
+    })
+    return this
+}
+
+let hoursWorkedOnDate = function(dateRequested){
+let inEvent = this.timeInEvents.find(function(e){
+ return e.date === dateRequested;
+  })
+ let outEvent = this.timeOutEvents.find(function(e){
+  return e.date === dateRequested;
+    })
+ return (outEvent.hour - inEvent.hour) / 100;
+}
+
+let wagesEarnedOnDate = function(dateRequested){
+let wage = hoursWorkedOnDate.call(this, dateRequested) * this.payPerHour
+return parseFloat(wage.toString())
+}
+
+let findEmployeeByFirstName = function(employees, firstName){
+return employees.find(function(record){
+ return record.firstName === firstName
+  })
+}
+
+let calculatePayroll = function(employees){
+return employees.reduce(function(memo, rec){
+ return memo + allWagesFor.call(rec)
+ }, 0)
+}
